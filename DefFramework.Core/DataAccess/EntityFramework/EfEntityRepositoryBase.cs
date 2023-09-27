9fe224d26c3c768c8b7e7b126cd1c17ee44c0e34
haliko <halilmertdeveliii@gmail.com>
2023-09-27 15:38:03 +0300

    Nhibernate repository base imp has been complated

--- DefFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ DefFramework.Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,7 +1,7 @@
 ﻿using DevFramework.Core.Entities;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
