ea8801bcd1df431899844ec93a4422d1c741bd02
haliko <halilmertdeveliii@gmail.com>
2023-09-27 15:08:59 +0300

    EfQuearybleRepositoryBase Imp has done

---
+++ DefFramework.Core/DataAccess/EntityFramework/EfQueryableRepositoryBase.cs
@@ -0,0 +1,24 @@
+﻿using DevFramework.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFramework.Core.DataAccess.EntityFramework
+{
+    public class EfQueryableRepositoryBase<T> : IQueryableRepository<T> where T : class, IEntity, new()
+    {
+        private DbContext _dbContext;
+        private IDbSet<T> _entities;
+
+        public EfQueryableRepositoryBase(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public IQueryable<T> Table { get; }
+    }
+}
